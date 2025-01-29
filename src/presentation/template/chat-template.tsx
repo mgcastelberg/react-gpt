ffc2859f9221755e874e98eec46d02295c36e6b3
ManuxDark <mgcastelberg@hotmail.com>
2025-01-29 09:42:06 -0600

    Funcionalidad de mensajes

--- src/presentation/template/chat-template.tsx
+++ src/presentation/template/chat-template.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react"
-import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components"
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from "../components";
 
 interface Message {
   text: string;
   isGPT: boolean;
 }
 
-export const OrthographyPage = () => {
+export const ChatTemplate = () => {
 
   const [isLoading, setIsLoading] = useState(false);
   const [messages, setMessages] = useState<Message[]>([]);
